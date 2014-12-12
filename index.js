062ec5fccc8a9d1af9b8c421e313c298815d94a2
Max Gfeller <max.gfeller@gmail.com>
2014-12-12 13:53:31 +0100

    Initial implementation

---
+++ index.js
@@ -0,0 +1,23 @@
+var cuid = require('cuid')
+var css = require('css')
+var insert = require('insert-css')
+
+var globals = ['*', 'body', 'html']
+
+module.exports = function(scope, style) {
+  var id = cuid.slug()
+  scope.setAttribute('data-scoped-css', id)
+
+  var o = css.parse(style)
+  o.stylesheet.rules.forEach(function(rule, i) {
+    var s = o.stylesheet.rules[i].selectors[0]
+    var globalAttr = false
+    if (globals.indexOf(s) > -1) globalAttr = true
+    var selector = globalAttr ?
+      s + ' [data-scoped-css="' + id + '"]'
+      : '[data-scoped-css="' + id + '"] ' + s
+    o.stylesheet.rules[i].selectors[0] = selector
+  })
+
+  insert(css.stringify(o))
+}
