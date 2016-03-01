232516fa326f14c83472f6b50250e7670d1e8e75
Sergey Filyuk <sergey.filyuk@zoomdata.com>
2016-03-01 18:21:32 +0200

    Improve scoped css to process array of selectors instead only first

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ var insert = require('insert-css')
 var globals = ['*', 'body', 'html']
 
 module.exports = function(scope, style) {
-  var id = cuid.slug()
-  scope.setAttribute('data-scoped-css', id)
+  var id = cuid.slug();
+  scope.setAttribute('data-scoped-css', id);
 
-  var o = css.parse(style)
+  var o = css.parse(style);
   o.stylesheet.rules.forEach(function(rule, i) {
     if (o.stylesheet.rules[i].type !== 'rule') return
 
-    var s = o.stylesheet.rules[i].selectors[0]
-    var globalAttr = false
-    if (globals.indexOf(s) > -1) globalAttr = true
-    var selector = globalAttr ?
+    var selectors = o.stylesheet.rules[i].selectors;
+    var globalAttr = false;
+
+    selectors.forEach(function(s,idx){
+      if (globals.indexOf(s) > -1) globalAttr = true
+      var selector = globalAttr ?
       s + ' [data-scoped-css="' + id + '"]'
-      : '[data-scoped-css="' + id + '"] ' + s
-    o.stylesheet.rules[i].selectors[0] = selector
+          : '[data-scoped-css="' + id + '"] ' + s;
+      o.stylesheet.rules[i].selectors[idx] = selector;
+    })
+
   })
 
-  insert(css.stringify(o))
+  insert(css.stringify(o));
 }
