a7c2fe2a3fe6666c0a4f2f398248b13f3f735472
Sergey Filyuk <sergey.filyuk@zoomdata.com>
2016-03-10 17:12:42 +0200

    remove semicolons return to author code style

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ var insert = require('insert-css')
 var globals = ['*', 'body', 'html']
 
 module.exports = function(scope, style) {
-  var id = cuid.slug();
-  scope.setAttribute('data-scoped-css', id);
+  var id = cuid.slug()
+  scope.setAttribute('data-scoped-css', id)
 
-  var o = css.parse(style);
+  var o = css.parse(style)
   o.stylesheet.rules.forEach(function(rule, i) {
     if (o.stylesheet.rules[i].type !== 'rule') return
 
-    var selectors = o.stylesheet.rules[i].selectors;
-    var globalAttr = false;
+    var selectors = o.stylesheet.rules[i].selectors
+    var globalAttr = false
 
     selectors.forEach(function(s,idx){
       if (globals.indexOf(s) > -1) globalAttr = true
       var selector = globalAttr ?
       s + ' [data-scoped-css="' + id + '"]'
-          : '[data-scoped-css="' + id + '"] ' + s;
-      o.stylesheet.rules[i].selectors[idx] = selector;
+          : '[data-scoped-css="' + id + '"] ' + s
+      o.stylesheet.rules[i].selectors[idx] = selector
     })
-
   })
 
-  insert(css.stringify(o));
+  insert(css.stringify(o))
 }
